7f7d98b41d65698f59cc0736b9acde4440022e27
tuancandongsang <tuancandongsang@gmail.com>
2023-01-06 15:53:13 +0700

    aaaaa

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,39 @@
   </div>
 </template>
 
+<script>
+
+export default {
+  data() {
+    return {
+      isVertical: false,
+    }
+  },
+  mounted() {
+    // by default, screen is vertical or not?
+    const angle = window.orientation;
+    this.setVertical(angle);
+
+    window.addEventListener('orientationchange', this.getDimensions);
+  },
+  unmounted() {
+    window.removeEventListener('orientationchange', this.getDimensions);
+  },
+  methods: {
+    getDimensions(event) {
+      const { angle } = event.target.screen.orientation;
+      console.log('tuancan', angle);
+      this.setVertical(angle);
+    },
+    setVertical(angle) {
+      const VERTICAL_ANGLE = 90;
+      this.isVertical = (angle === VERTICAL_ANGLE);
+      console.log('demo 1', this.isVertical);
+    }
+  }
+}
+</script>
+
 <!-- <style>
 * {
   box-sizing: border-box;
