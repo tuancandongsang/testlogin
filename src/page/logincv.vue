a1bcd361be687c0667c1d2f6ef1d4743ca89a8ea
tuancan <tuacandongsang@gmail.com>
2023-02-01 00:37:11 +0700

    2.1

--- src/page/logincv.vue
+++ src/page/logincv.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="logincv">
     <div class="infor">
-      <img src="./imgcv/topcv-logo.webp" alt="" />
-      <h1>Chào mừng bạn trở lại,</h1>
-      <p>
-        Cùng xây dựng một hồ sơ nổi bật và nhận được các cơ hội sự nghiệp lý
-        tưởng
-      </p>
+      <div class="infor-header">
+        <a href="#"><img src="./imgcv/topcv-logo.webp" alt="" /></a>
+        <h2>Chào mừng bạn trở lại,</h2>
+        <p>
+          Cùng xây dựng một hồ sơ nổi bật và nhận được các cơ hội sự nghiệp lý
+          tưởng
+        </p>
+      </div>
+      <div class="infor-contain">
+        <email />
+        <div class="infor-contain_login">
+          <button class="infor-contain_login-submit">Đăng Nhập</button>
+          <p>Hoặc</p>
+          <div class="infor-contain_login-social">
+            <button class="facebook">Facebook</button>
+            <button class="google">Google</button>
+            <button class="linkedin">Linkedin</button>
+          </div>
+        </div>
+        <div class="infor-contain_forget">
+          <p>
+            Bạn chưa có tài khoản? <a href="#"><B>Đăng ký ngay</B></a>
+          </p>
+          <p>
+            <a href="#"><b>Quên mật khẩu</b></a>
+          </p>
+        </div>
+        <div>
+          <p><b>Bạn gặp khó khăn khi tạo tài khoản?</b></p>
+          <p>Vui lòng gọi tới số (024) 6680 5588 (giờ hành chính).</p>
+        </div>
+      </div>
     </div>
     <div class="carouse">
-      <daw class="alo" />
+      <daw />
     </div>
   </div>
 </template>
@@ -17,9 +43,12 @@
 <script>
 import './logincv.scss';
 import daw from './logincv/daw.vue';
+import email from './logincv/email.vue';
 export default {
   data() {},
   methods: {},
-  components: { daw },
+  components: { daw, email },
 };
 </script>
+<style lang="scss" scoped>
+</style>
--- src/page/logincv/daw.vue
+++ src/page/logincv/daw.vue
@@ -1,23 +1,76 @@
 <template>
-  <a-carousel autoplay>
-    <div><h3>1</h3></div>
-    <div><h3>2</h3></div>
-    <div><h3>3</h3></div>
-    <div><h3>4</h3></div>
-  </a-carousel>
+  <div class="login_right">
+    <a-carousel autoplay>
+      <div class="login_item">
+        <img src="../imgcv/img1_login.webp" alt="" />
+        <h4>Công cụ viết CV đẹp Miễn phí</h4>
+        <p>
+          Nhiều mẫu CV đẹp, phù hợp nhu cầu ứng tuyển các vị trí khác nhau. Dễ
+          dàng chỉnh sửa thông tin, tạo CV online nhanh chóng trong vòng 5 phút.
+        </p>
+      </div>
+      <div class="login_item">
+        <img src="../imgcv/img2_login.webp" alt="" />
+        <h4>Bảo mật & An toàn tuyệt đối</h4>
+        <p>
+          Bạn có thể chủ động bật / tắt trạng thái tìm việc. Nếu trạng thái tắt,
+          không ai có thể xem được CV của bạn.
+        </p>
+      </div>
+      <div class="login_item">
+        <img src="../imgcv/img3_login.webp" alt="" />
+        <h4>Hỗ trợ Người tìm việc Nhà</h4>
+        <p>
+          tuyển dụng chủ động tìm kiếm và liên hệ với bạn qua hệ thống kết nối
+          ứng viên thông minh.
+        </p>
+      </div>
+    </a-carousel>
+  </div>
 </template>
   
-  <style scoped>
-/* For demo */
+<style scoped lang="scss">
 .ant-carousel :deep(.slick-slide) {
   text-align: center;
-  height: 160px;
-  line-height: 160px;
-  background: #364d79;
+  height: 100%;
   overflow: hidden;
 }
 
 .ant-carousel :deep(.slick-slide h3) {
   color: #fff;
+}
+.login_right {
+  padding-top: 100px;
+  width: 100%;
+  .login_item {
+    display: flex;
+    // justify-content: center;
+
+    img {
+      width: 60%;
+      padding: 0 20px 20px 20px;
+      display: inline-block;
+    }
+    h4 {
+      color: #212f3f;
+      font-size: 24px;
+      color: #212f3f;
+      letter-spacing: 0;
+      font-weight: 500;
+      line-height: 1.2;
+      margin-bottom: 0.5rem;
+      margin-top: 0;
+    }
+    p {
+      color: rgba(0, 0, 0, 0.851);
+      font-size: 16px;
+      letter-spacing: 0;
+      margin-bottom: 1rem;
+      margin-top: 0;
+      width: 60%;
+      word-wrap: break-word;
+      display: inline-block;
+    }
+  }
 }
 </style>
